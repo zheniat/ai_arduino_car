51e437ff8bc0f9c98baef519e11f494433a5cf67
Eugene Citrisky <eugene.citrisky@gmail.com>
2018-02-23 21:49:39 -0800

    Added integration with XBee

--- ai_car_control/ai_car_control.ino
+++ ai_car_control/ai_car_control.ino
@@ -1,22 +1,8 @@
+/* MOTOR PINOUT*/
 int encoder_pin_right = 2 ; // pulse output from the module
 int encoder_pin_left = 3 ; // pulse output from the module
 
-unsigned int rpm_left; // rpm reading
-unsigned int rpm_right; // rpm reading
-
-volatile byte pulses_left; // number of pulses
-volatile byte pulses_right; // number of pulses
-
-unsigned long timeold;
-
 unsigned int motorspeed = 90; //from 0 to 255
-unsigned int interrupt_left = 0;
-unsigned int interrupt_right = 1;
-
-// number of pulses per revolution
-// based on your encoder disc
-unsigned int pulsesperturn = 20;
-unsigned int time_gap = 1000;
 
 //identifies whether the wheel is rotating forward (True) or backward (False)
 boolean left_forward = false;
@@ -38,8 +24,8 @@ const int front_left_echo = 13;
 // L9110 connections
 #define L9110_1A 10 // Pin D10 --> Motor B Input A
 #define L9110_1B 11 // Pin D11 --> Motor B Input B
-#define L9110_2A 6 // Pin D6 --> Motor B Input A
-#define L9110_2B 7 // Pin D7 --> Motor B Input B
+#define L9110_2A 4 // Pin D6 --> Motor B Input A
+#define L9110_2B 5 // Pin D7 --> Motor B Input B
 
 
 #define MOTOR_LEFT_PWM L9110_2A // Motor PWM Speed
@@ -48,7 +34,19 @@ const int front_left_echo = 13;
 #define MOTOR_RIGHT_PWM L9110_1A // Motor PWM Speed
 #define MOTOR_RIGHT_DIR L9110_1B // Motor Direction
 
+/* END MOTOR PINOUT */
 
+/* WHEEL ROTATION CONTROL*/
+
+unsigned long timeold;
+
+volatile byte pulses_left; // number of pulses
+volatile byte pulses_right; // number of pulses
+
+// number of pulses per revolution
+// based on your encoder disc
+unsigned int pulsesperturn = 20;
+unsigned int time_gap = 1000;
 
 void counter_left()  // counts from the speed sensor
 {
@@ -60,9 +58,36 @@ void counter_right()  // counts from the speed sensor
   pulses_right++;  // increase +1 the counter value
 } 
 
+
+/* END WHEEL ROTATION CONTROL*/
+
+/* CONTROL DATA */
+//Data controlling motor rotation, received from the server
+//Data format: <100,-1, +1>  - duration ms, left wheel direction (+1 - forward, -1 backward), right wheel direction
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];        // temporary array for use when parsing
+
+ // variables to hold the parsed data
+char receivedMessage[numChars] = {0};
+int duration = 0;
+int leftWheel = 0;
+int rightWheel = 0;
+
+boolean newData = false;
+
+/*END CONTROL DATA*/
+
+/*DRIVE DURATION*/
+unsigned long time_start_drive = 0; 
+unsigned long car_drive_time = 0;
+
+/*END DRIVE DURATION*/
+
 void setup() 
 {
-  Serial.begin(38400);
+  Serial.begin(9600);
   
   pinMode(encoder_pin_left, INPUT);
   pinMode(encoder_pin_right, INPUT);
@@ -82,15 +107,10 @@ void setup()
   pinMode(front_left_echo, INPUT);
   pinMode(back_right_echo, INPUT);
   pinMode(back_left_echo, INPUT);
-  
-  motor_reset();
 
-   // Initialize
+  // Initialize
    pulses_left = 0;
    pulses_right = 0;
-   
-   rpm_left = 0;
-   rpm_right = 0;
    
    timeold = 0;
    
@@ -99,66 +119,153 @@ void setup()
    attachInterrupt(0, counter_right, FALLING);
    attachInterrupt(1, counter_left, FALLING);
 
-    motor_reset();
-    //l_forward(200);
-    //r_back(100);
+   time_start_drive = 0;
+   car_drive_time = -1;
 
+  
+  motor_reset();
 }
 
 void loop()
-{
-  get_sensor_data();
-  
-  if(Serial.available()){ // Checks whether data is comming from the serial port
-      char state = (char)Serial.read(); // Reads the data from the serial port
-      Serial.println("BT state: ");
-      Serial.print(state);
-  } 
+{ 
+  //Check for new commands from the serial port
+  recvDataWithStartEndMarkers();
+  if (newData == true) {
+      strcpy(tempChars, receivedChars);
+          // this temporary copy is necessary to protect the original data
+          //   because strtok() used in parseData() replaces the commas with \0
+      parseDataAndDrive();      
+      newData = false;
+   }
+   stopCarAndReport();
 }
 
+void stopCarAndReport(){
+  if (car_drive_time > 0 && (millis() - time_start_drive >= car_drive_time)) {
+    car_drive_time = -1;
+    motor_reset();
+    String result = get_sensor_data();
+    Serial.print(result);
+  }
+}
 
-void get_sensor_data(){
+void recvDataWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
 
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void parseDataAndDrive() {      // split the data into its parts
+
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars,",");// get the first part - duration
+    duration = atoi(strtokIndx);     // convert this part to an integer
+
+    if(duration<=0 || duration>30000){
+      duration = 1;
+    }
+    
+    strtokIndx = strtok(NULL, ","); //get the second part - left wheel
+    leftWheel = atoi(strtokIndx);     // convert this part to an integer
+    
+    strtokIndx = strtok(NULL, ","); //get the second part - right wheel
+    rightWheel = atoi(strtokIndx);  // convert to an integer
+
+    motor_reset();
+    
+    if(leftWheel>0){
+      l_forward();
+    } else if(leftWheel<0) {
+      l_back();
+    }
+
+    if(rightWheel>0){
+      r_forward();
+    } else if(rightWheel<0){
+      r_back();
+    }
+    //start dring car for the duration
+    time_start_drive = millis();
+    car_drive_time = duration;
+}
+
+//Returns sensor data
+//Format: <direction_left, direction_right, rpm_left, rpm_right, dist_front_right, dist_front_left, dist_back_right, dist_back_left>
+//Example: <1, -1, 140, 150, 10, 11, 85, 90> - left rpm 140 forward, right rpm 150 backward, distances: 10, 11, 85, and 90
+String get_sensor_data(){
+  String result = "<";
   if (millis() - timeold >= time_gap) {
-      //Don't process interrupts during calculations
-      detachInterrupt(0);
-      detachInterrupt(1);
 
-      rpm_left = (60 * time_gap / pulsesperturn )/ (millis() - timeold)* pulses_left;
-      rpm_right = (60 * time_gap / pulsesperturn )/ (millis() - timeold)* pulses_right;
-      timeold = millis();
-      pulses_left = 0;
-      pulses_right = 0;
-      Serial.println("RPM left = ");
-      Serial.println(rpm_left,DEC);
-      Serial.println("Direction left forward = ");
-      Serial.println(left_forward,DEC);
-      Serial.print("RPM right = ");
-      Serial.println(rpm_right,DEC);
-      Serial.println("Direction right forward = ");
-      Serial.println(right_forward,DEC);
+    //Don't process interrupts during calculations
+    detachInterrupt(0);
+    detachInterrupt(1);
+
+    int dir_l = left_forward ? 1 : -1;
+    result += dir_l;
+    result += ',';
+    int dir_r = right_forward ? 1 : -1;
+    result += dir_r;
+    result += ',';
+    float time_diff = millis() - timeold;
+    
+    float rpm_l = (60 * time_gap / pulsesperturn )/ time_diff * pulses_left;
+    float rpm_r = (60 * time_gap / pulsesperturn )/ time_diff * pulses_right;
+    
+    result +=  roundf(10 * rpm_l) / 10;
+    result += ',';
+    result += roundf(10 * rpm_r) / 10;
+    result += ',';
+    
+    timeold = millis();
+    pulses_left = 0;
+    pulses_right = 0;
       
-      //Restart the interrupt processing
-      attachInterrupt(0, counter_left, FALLING);
-      attachInterrupt(1, counter_right, FALLING);
-      
-      long dist_fr = get_distance(front_right_trig, front_right_echo);
-      Serial.print("Distance FR: ");
-      Serial.println(dist_fr);
+    //Restart the interrupt processing
+    attachInterrupt(0, counter_left, FALLING);
+    attachInterrupt(1, counter_right, FALLING);
 
-      long dist_fl = get_distance(front_left_trig, front_left_echo);
-      Serial.print("Distance FL: ");
-      Serial.println(dist_fl);
-
-      long dist_bl = get_distance(back_left_trig, back_left_echo);
-      Serial.print("Distance BL: ");
-      Serial.println(dist_bl);
-
-      long dist_br = get_distance(back_right_trig, back_right_echo);
-      Serial.print("Distance BR: ");
-      Serial.println(dist_br);
-
-   }  
+    long dist_fr = get_distance(front_right_trig, front_right_echo);
+    long dist_fl = get_distance(front_left_trig, front_left_echo);
+    long dist_bl = get_distance(back_left_trig, back_left_echo);
+    long dist_br = get_distance(back_right_trig, back_right_echo);
+    
+    result += dist_fr;
+    result += ',';
+    result += dist_fl;
+    result += ',';
+    result += dist_br;
+    result += ',';
+    result += dist_bl;
+  }
+  result += +">";
+  return result;
 }
 
 
@@ -200,30 +307,29 @@ int checkSpeed(int speed){
 }
 
 
-
-void l_forward(int speed){
+void l_forward(){
     left_forward = true;
     digitalWrite( MOTOR_LEFT_DIR, LOW ); 
-    analogWrite( MOTOR_LEFT_PWM, checkSpeed(speed)); 
+    digitalWrite( MOTOR_LEFT_PWM, HIGH ); 
 }
 
-void l_back(int speed){ 
+void l_back(){ 
     left_forward = false;
     digitalWrite( MOTOR_LEFT_DIR, HIGH ); 
-    analogWrite( MOTOR_LEFT_PWM, checkSpeed(speed)); 
+    digitalWrite( MOTOR_LEFT_PWM, LOW ); 
 }
 
 
-void r_forward(int speed){ 
+void r_forward(){ 
   right_forward = true;
-  digitalWrite( MOTOR_RIGHT_DIR, HIGH ); 
-  analogWrite( MOTOR_RIGHT_PWM, checkSpeed(speed)); 
+  digitalWrite( MOTOR_RIGHT_DIR, HIGH); 
+  digitalWrite( MOTOR_RIGHT_PWM, LOW); 
 }
 
 
-void r_back(int speed){ 
+void r_back(){ 
   right_forward = false;
   digitalWrite( MOTOR_RIGHT_DIR, LOW ); 
-  analogWrite( MOTOR_RIGHT_PWM, checkSpeed(speed)); 
+  digitalWrite( MOTOR_RIGHT_PWM, HIGH); 
 }
 
